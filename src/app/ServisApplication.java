30a9008d2944e52b84d5dae5056888465a7fc776
MLazarevic36 <hrle96ab@gmail.com>
2020-05-15 21:34:14 +0200

    dodato ucitavanje automobila

--- src/app/ServisApplication.java
+++ src/app/ServisApplication.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 
 
 import model.Administrator;
+import model.Automobil;
+import model.Marka_auta;
+import model.Model_auta;
+import model.Musterija;
 
 public class ServisApplication {
 
@@ -37,6 +41,60 @@ public class ServisApplication {
 			brAdmin.close();
 		}
 		
+		
+		FileReader frMus = new FileReader("C:\\Users\\hrle9\\eclipse-workspace\\ServisApplication\\src\\app\\musterije");
+		BufferedReader brMus = new BufferedReader(frMus);
+
+		try {
+		String fileLine = null;
+		
+		while ((fileLine =  brMus.readLine()) != null) {
+			String[] fields = fileLine.split(",");
+				
+			Musterija musterija = new Musterija();
+			musterija.setIme(fields[0]);
+			musterija.setPrezime(fields[1]);
+			musterija.setKorisnicko_ime(fields[2]);
+			musterija.setLozinka(fields[3]);
+			musterija.setAdresa(fields[4]);
+			musterija.setBroj_telefona(fields[5]);
+			musterija.setJMBG(fields[6]);
+			musterija.setBroj_sakupljenih_nagradnih_bodova(fields[7]);
+			musterija.setPol(fields[8]);
+			
+			System.out.println(musterija);
+		}
+		} finally {
+			brMus.close();
+		}
+		
+		FileReader frAuto = new FileReader("C:\\Users\\hrle9\\eclipse-workspace\\ServisApplication\\src\\app\\automobili");
+		BufferedReader brAuto = new BufferedReader(frAuto);
+
+		try {
+		String fileLine = null;
+		
+		while ((fileLine =  brAuto.readLine()) != null) {
+			String[] fields = fileLine.split(",");
+				
+			
+			Musterija vlasnik = new Musterija();
+			vlasnik.setPrezime(fields[0]);
+			
+			Automobil auto = new Automobil();
+			auto.setVlasnik(vlasnik);
+			auto.setMarka(Marka_auta.valueOf(fields[1]));
+			auto.setModel(Model_auta.valueOf(fields[2]));
+			auto.setGodina_proizvodnje(Integer.parseInt(fields[3]));
+			auto.setZapremina_motora(fields[4]);
+			auto.setSnaga_motora(fields[5]);
+			auto.setVrsta_goriva(fields[6]);
+			
+			System.out.println(auto);
+		}
+		} finally {
+			brAuto.close();
+		}
 		
 	}
 
